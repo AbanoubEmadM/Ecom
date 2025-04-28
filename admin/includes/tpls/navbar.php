75633c82f5918910d198772b53c5c3e373bd9237
AbanoubEmadM <emadbebo729@gmail.com>
2025-04-29 02:56:51 +0300

    add logout.php

--- admin/includes/tpls/navbar.php
+++ admin/includes/tpls/navbar.php
@@ -29,7 +29,7 @@
                     <a class="dropdown-item" href="#"><?php echo lang('EDIT_PROFIELS') ?></a>
                     <a class="dropdown-item" href="#"><?php echo lang('SETTINGS') ?></a>
                     <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#"><?php echo lang('LOGOUT') ?></a>
+                    <a class="dropdown-item" href="logout.php"><?php echo lang('LOGOUT') ?></a>
                 </div>
             </li>
         </ul>
